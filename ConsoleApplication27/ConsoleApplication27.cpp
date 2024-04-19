d6eb0c0fa7e077c980d49cfeccb6388fb30345cc
Damir <Damir@DESKTOP-ELD43TT>
2024-04-19 23:04:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication27/ConsoleApplication27.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double computeSequenceTerm(int n) {
+    if (n == 1) {
+        return 10;
+    }
+    else {
+        return 5 / computeSequenceTerm(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    double term = computeSequenceTerm(n);
+    cout << "n member of the series: " << term << endl;
+
+    return 0;
+}
